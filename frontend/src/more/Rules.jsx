9bece159124b1cd26d7932f89a7db21f5b9ddb09
LuuDucKhanh2022 <luuduckhanharsenal@.com>
2023-01-11 12:59:38 +0700

    add build folder

--- frontend/src/more/Rules.jsx
+++ frontend/src/more/Rules.jsx
@@ -25,7 +25,7 @@ const Rules = () => {
                     fontFamily:"Roboto",
                 }}>Some Rules:</span>
                 <li>1. You can easily return your product..But you have to give us the delivery charge...</li>
-                <li>2. You have to give delivery charge first for cash on Delivery..In Los Angeles City you have to give 70tk and outside jashore charge will be 120 tk</li>
+                <li>2. You can customize your name, jersey number, tournament logo when ordering products in the Kit category</li>
                 <li>3. You can not buy the outofstock products...</li>
                 <li>4. You can buy any products from us...we are trying to our best for give the best quality of products...</li>
                 <li>5. You can find more new features in our buiseness in very soon...Our developers team always work for your good services...</li>
